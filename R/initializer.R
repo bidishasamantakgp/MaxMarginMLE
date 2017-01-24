b26806f330c40fa6fcb7d7ddd40e5e4fb0bea2d3
bidishasamantakgp <bidisha.samanta@gmail.com>
2017-01-24 20:00:15 +0530

    Adding initialiser

---
+++ R/initializer.R
@@ -0,0 +1,39 @@
+library(lpSolveAPI)
+
+getinitials<-function(constraintslist,nc,sc){
+
+	vc <- nc+ sc
+	lps.model <- make.lp(0, vc) # define 3 variables, the constraints are added below
+	r <- length(rankList)
+	for(i in 1:sc){
+		constraint <- NULL
+		#for(j in 1:vc){
+		#	constraint <- c(constraint, constraintslist[i+(j-1)*r])
+		#}
+		#print(constraint)
+		#add.constraint(lps.model, constraint, "<=", -1)
+		add.constraint(lps.model, constraintslist[[i]], ">=", 1)
+	}
+
+	#lower bound for every variable
+	#add.constraint(lps.model, c(rep(c(0),nc),rep(c(1),sc)), "=", 1)
+	for(i in 1:nc){
+		sl <- rep(c(0), each=vc)
+		sl[i] = 1
+		add.constraint(lps.model, sl, ">",0.001)
+	}
+
+	#lower and upper bound for slack variables
+	#sl <- rep(c(0), each=vc)
+	for(i in (nc+1):vc){
+		sl <- rep(c(0), each=vc)
+		sl[i] = 1
+		add.constraint(lps.model, sl, "<",0.9999999)
+		add.constraint(lps.model, sl, ">=",0.00000001)
+	}
+	#add.constraint(lps.model, sl, ">=",1)
+	# set objective function (default: find minimum)
+	set.objfn(lps.model, rep(c(1), each=vc))  
+	solve(lps.model)
+	return(get.variables(lps.model))
+}
